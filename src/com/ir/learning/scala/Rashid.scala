b3dd7d4c71c63a5af24e9e38a734c6abc2b678e8
ibrahimgit <ibrahim.rshd@gmail.com>
2017-09-22 00:45:46 +0530

    learning scala

--- src/com/ir/learning/scala/Rashid.scala
+++ src/com/ir/learning/scala/Rashid.scala
@@ -2,6 +2,23 @@ package com.ir.learning.scala
 
 object Rashid {
   def main(args: Array[String]): Unit = {
+    var value : Int = 5
+    val constantValue : Int = 10
+    var value2 = 88
+    var doubleVal = 2.0;
+    //strVal = ""
+    
     println("Hello Saba");
+    println("Value: " + value);
+    value = 8
+    //sconstantValue = 17; // not possible
+    
+    callme("hello", value);
   }
+  
+  def callme(stringValue : String, value : Int): Unit = {
+    println(stringValue);
+    println(value);
+  }
+  
 }
