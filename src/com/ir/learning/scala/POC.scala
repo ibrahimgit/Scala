9f40c56d74c9e2b6b4dea8d47b3a419c9e645bf5
ibrahimgit <ibrahim.rshd@gmail.com>
2017-09-24 22:56:04 +0530

    POC

---
+++ src/com/ir/learning/scala/POC.scala
@@ -0,0 +1,15 @@
+package com.ir.learning.scala
+
+class POC(x : Int, str :String) {
+  
+  var val1 = x;
+  var val2 = str;
+  
+  def doStuff(v :Int, str :String): Unit = {
+    
+    println("Value: " + v+x);
+    println("String: " + str + " Value: " + val2);
+    
+  }
+  
+}
